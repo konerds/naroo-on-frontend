99ba4993c22becf0ed4219f41e94eb098eac39b6
jjw247 <adr10won@gmail.com>
2023-01-08 07:36:31 +0900

    again

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,23 @@ module.exports = {
   content: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     screens: {
+      ...defaultTheme.screens,
       xxs: '0px',
       xs: '420px',
       lg: '1024px',
       xl: '1200px',
-      ...defaultTheme.screens,
     },
     extend: {
       colors: {
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet,
         gray: {
+          ...colors.neutral,
           100: '#C4C4C4',
           200: '#666666',
           300: '#555555',
           400: '#111111',
-          ...colors.neutral,
         },
         'shuttle-gray': '#5E6774',
         harp: '#E9EFF1',
